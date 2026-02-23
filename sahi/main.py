dd3443fe834daaeec0eee675f98497f13f46e282
raunitsingh <raunitsingh33@gmail.com>
2026-02-24 00:27:29 +0530

    sync from ubuntu

---
+++ sahi/main.py
@@ -0,0 +1,37 @@
+import cv2
+from engine.sahi_object_detection import SAHIObjectDetection
+
+
+sahi_od = SAHIObjectDetection("models/yolo11m.pt")
+
+
+cap = cv2.VideoCapture("yt7.mp4")
+
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+
+    predictions = sahi_od.detect(frame)
+    for pred in predictions:
+        bbox = pred.bbox
+        class_id = pred.category.id
+        score = pred.score.value
+
+
+        x, y, x2, y2 = bbox.minx, bbox.miny, bbox.maxx, bbox.maxy
+        cv2.rectangle(frame, (x, y), (x2, y2), sahi_od.colors[class_id], 3)
+
+
+    frame = cv2.resize(frame, dsize= None, fx=0.5, fy=0.5)
+    cv2.imshow("Frame", frame)
+    key =  cv2.waitKey(1) 
+    if key == 27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
